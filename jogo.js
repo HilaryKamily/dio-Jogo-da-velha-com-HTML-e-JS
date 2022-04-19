6c6f964d18108ab268305c4e6c254fda2dba3f1e
Hílary <hilarykamily@gmail.com>
2022-04-19 20:07:42 -0300

    Muda o jogador e faz com que só seja marcado uma vez

--- jogo.js
+++ jogo.js
@@ -8,8 +8,20 @@ function escolherQuadrado(id){
     
     var quadrado = document.getElementById(id);
 
+    if(quadrado.innerHTML !== '-'){
+        return;
+    }
+
     quadrado.innerHTML = jogador;
     quadrado.style.color = '#000';
+
+    if(jogador === 'X'){
+        jogador = 'O';
+    } else{
+        jogador = 'X';
+    }
+
+    mudarJogador(jogador)
 }
 
 function mudarJogador(valor){
